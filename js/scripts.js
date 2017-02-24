a35ffe855585499b0de332a97ba7c53efb6cea8a
ken-peter <Ken Peter>
2017-02-24 11:38:24 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var input = prompt();
+for (var i = 1; <=parseInt(input); i++ ){
+if (i % 3 === 0){
+if (i % 5 === 0){
+console.log("pingpong");
+}
+else{
+console log("ping");
+}
+}
+else if (i % 5 === 0){
+console.log("pong");
+}
+else {
+console.log(i);
+}
+}
