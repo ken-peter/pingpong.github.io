fd83371d693954e7720a801082520acc3c75b3d2
ken-peter <Ken Peter>
2017-02-24 14:27:55 +0300

    adding javascript and bootstrap changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,24 @@
-var input = prompt();
-for (var i = 1; <=parseInt(input); i++ ){
-if (i % 3 === 0){
-if (i % 5 === 0){
-console.log("pingpong");
-}
-else{
-console log("ping");
-}
-}
-else if (i % 5 === 0){
-console.log("pong");
-}
-else {
-console.log(i);
-}
-}
+$(document).ready(function() {
+    $("form").submit(function(event) {
+
+        event.preventDefault();
+        $("#list").empty();
+
+        var input = parseInt($('#userinput').val());
+
+        for (var i = 1; i <= input; i++) {
+            if (i % 3 === 0) {
+                if (i % 5 === 0) {
+                    $('#list').append('<li>pingpong</li>');
+                } else {
+                    $('#list').append('<li>ping</li>');
+                }
+            } else if (i % 5 === 0) {
+                $('#list').append('<li>pong</li>');
+            } else {
+                $('#list').append("<li>" + i + "</li>");
+            }
+        }
+ $("#results").show();
+    });
+});
